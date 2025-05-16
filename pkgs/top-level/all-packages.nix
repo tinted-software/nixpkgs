ef7d1107d21adcd846d504de9ed72c267ee8b42c
Tristan Ross <tristan.ross@midstall.com>
2025-05-16 15:08:18 -0700

    llvmPackages: llvmPackages_19 -> llvmPackages_20

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5815,7 +5815,7 @@ with pkgs;
   libllvm = llvmPackages.libllvm;
   llvm-manpages = llvmPackages.llvm-manpages;
 
-  llvmPackages = llvmPackages_19;
+  llvmPackages = llvmPackages_20;
 
   inherit
     (rec {
