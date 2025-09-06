cde7e795554423d758293719577b6b6b7bb3ff5d
Theo Paris <theo@theoparis.com>
2025-09-06 13:08:32 -0700

    sdl3: disable ibus on android targets

--- pkgs/by-name/sd/sdl3/package.nix
+++ pkgs/by-name/sd/sdl3/package.nix
@@ -47,7 +47,7 @@
   alsaSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
   dbusSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
   drmSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
-  ibusSupport ? stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin,
+  ibusSupport ? stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin && !stdenv.hostPlatform.isAndroid,
   jackSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
   libdecorSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
   libudevSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
